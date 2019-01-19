edd0ae10334ec62ab411d8235be83875cd53167e
ardsj <ardsj@yahoo.com.br>
2019-01-19 17:41:17 -0200

    Configurando databinding

--- app/src/main/java/com/mofaia/mypay/MainActivity.kt
+++ app/src/main/java/com/mofaia/mypay/MainActivity.kt
@@ -2,11 +2,13 @@ package com.mofaia.mypay
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import com.mofaia.mypay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }

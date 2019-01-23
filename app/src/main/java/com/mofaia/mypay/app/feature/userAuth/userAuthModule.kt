4daade3a075b47585ed771bd495512c45117ffa8
ardsj <ardsj@yahoo.com.br>
2019-01-23 20:02:11 -0200

    Renomeando modulo

--- app/src/main/java/com/mofaia/mypay/app/feature/userAuth/userAuthModule.kt
+++ app/src/main/java/com/mofaia/mypay/app/feature/userAuth/userAuthModule.kt
@@ -3,6 +3,6 @@ package com.mofaia.mypay.app.feature.userAuth
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
-val authenticationModule = module {
-    viewModel { CreateAccountViewModel(get()) }
+val userAuthModule = module {
+    viewModel { UserAuthViewModel(get()) }
 }

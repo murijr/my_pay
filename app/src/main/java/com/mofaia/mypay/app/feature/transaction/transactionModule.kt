12a6ad3510eb81004b0858ffc110e907409cb0fd
ardsj <ardsj@yahoo.com.br>
2019-01-26 23:48:21 -0200

    configurando modulo

--- app/src/main/java/com/mofaia/mypay/app/feature/transaction/transactionModule.kt
+++ app/src/main/java/com/mofaia/mypay/app/feature/transaction/transactionModule.kt
@@ -1,2 +1,10 @@
 package com.mofaia.mypay.app.feature.transaction
 
+import com.mofaia.mypay.app.common.CurrencyConverter
+import com.mofaia.mypay.app.constant.WALLET_REPOSITORY
+import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+
+val transactionModule = module {
+    viewModel { TransactionViewModel(get(WALLET_REPOSITORY), get()) }
+}
